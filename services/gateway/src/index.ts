e94b805e3a2f126af977c498620a5f3a7600bbd5
Vittorio Adamo <vittorio.adamo@adidas.com>
2019-11-28 16:54:42 +0100

    #3 Add gateway service

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -1,9 +1,24 @@
+import { ApolloGateway } from "@apollo/gateway";
+import { ApolloServer } from "apollo-server-fastify";
+
 import { Server } from "@lazy-graphql/shared";
 import Configuration from "./lib/Configuration";
 
-const { SERVER_PORT, SERVER_ADDRESS } = Configuration;
+const { SERVER_PORT, SERVER_ADDRESS, USER_SERVICE_URL, VIDEOGAME_SERVICE_URL } = Configuration;
 
-const server = new Server(SERVER_PORT, SERVER_ADDRESS);
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: "user", url: USER_SERVICE_URL },
+    { name: "videogame", url: VIDEOGAME_SERVICE_URL },
+  ],
+});
+
+const apolloServer = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
+
+const server = new Server(SERVER_PORT, SERVER_ADDRESS, apolloServer);
 
 server.start();
 server.log.info("⚡️ Graphql Gateway Service started");
--- services/gateway/src/lib/Configuration.ts
+++ services/gateway/src/lib/Configuration.ts
@@ -3,11 +3,15 @@ import { getNumberFromEnv, getEnvOrThrow, createConfiguration } from "@lazy-grap
 export interface Configuration {
   SERVER_PORT: number;
   SERVER_ADDRESS: string;
+  USER_SERVICE_URL: string;
+  VIDEOGAME_SERVICE_URL: string;
 }
 
 const serviceConfiguration: Configuration = {
   SERVER_PORT: getNumberFromEnv("SERVER_PORT"),
   SERVER_ADDRESS: getEnvOrThrow("SERVER_ADDRESS"),
+  USER_SERVICE_URL: getEnvOrThrow("USER_SERVICE_URL"),
+  VIDEOGAME_SERVICE_URL: getEnvOrThrow("VIDEOGAME_SERVICE_URL"),
 };
 
 export default createConfiguration<Configuration>(serviceConfiguration);
